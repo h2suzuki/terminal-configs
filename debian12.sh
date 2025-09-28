f6c188add904cd4b45f09784ecf37feb5cc0b538
Hideaki Suzuki <h2suzuki@gmail.com>
2025-09-28 13:49:06 +0900

    Import .rye/env before executing a rye command

--- debian12.sh
+++ debian12.sh
@@ -119,6 +119,7 @@ export RYE_INSTALL_OPTION="--yes"
 [ -s rye-get.sh ] ||
 run curl -o rye-get.sh -fsSL https://rye-up.com/get
 run bash ./rye-get.sh
+source "$HOME/.rye/env"
 run rye self completion '>' /usr/share/bash-completion/completions/rye
 
 
